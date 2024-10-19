515449fb6aeb742aaf58e2d2e00b6d9cc3ecfa75
Abdul Rafeh <abdulrafeh@live.co.uk>
2024-10-19 17:21:14 +0500

    testing

--- services/mail.php
+++ services/mail.php
@@ -4,7 +4,7 @@
   $email = $_POST['email'];
   $message = $_POST['message'];
   $formcontent="From: $name \n Message: $message";
-  $recipient = "ahsan.ihsan@outlook.com";
+  $recipient = "abdulrafeh@live.co.uk";
   $subject = "Contact Form";
   $mailheader = "From: $email \r\n";
 
